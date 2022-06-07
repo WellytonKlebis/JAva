865d5602fa5bcb4beec179df84b0b643c6d6d2c2
Wellyton Rodrigues da Silva Klebis <wellyton700@hotmail.com>
2022-06-06 22:13:16 -0300

    alteração pacote util.

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -3,39 +3,40 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+
 
 
 
 public class ConnectionFactory {
-  
+    
+    // Parametros para acesso ao meu banco de dados.
  public static final String DRIVER = "com.mysql.jdbd.Driver" ;
- public static final String URL = "jbdc:mysql://localhost:3306/todoApp";// endereço do meu banco de dados 
+ public static final String URL = "jbdc:mysql://localhost:3306/todoApp";
  public static final String USER = "root";
  public static final String PASS = ""; 
  
- 
- public static Connection getConnection(){ // Metodo retorna uma conexão,é estatico não preciso criar um objeto para usar essa função
- /* Dentro do tray coloco o codigo que pode gerar erro */
-      try { // forma de pegar erros que possam acontecer e fazer um tratamento.
-        Class.forName(DRIVER);// carrega o Driver 
-        return DriverManager.getConnection(URL, USER, PASS); //  extipula a conexão com banco de dados 
-     } catch (Exception ex){
-        throw new RuntimeException("Erro na conexão com o banco de dados ",ex ); 
- 
-  }
+     /* Metodo retorna uma conexão,
+     é estatico não preciso criar um objeto para usar essa função */
+ public static Connection getConnection(){
+
+     try {        
+          Class.forName(DRIVER);// carrega o Driver 
+          return DriverManager.getConnection(URL, USER, PASS); //  estipula a conexão com banco de dados 
+        }    
+         catch (Exception ex){
+         throw new RuntimeException("Erro na conexão com o banco de dados ",ex ); 
+         }
  
  }
  
  public static void closeConnection (Connection connection){
- try { 
-     
-     if(connection !=null){
-        connection.close();
-       
-     }
-     
- }  catch (Exception ex){ xxxxxxx
+     try {  
+        if(connection != null){
+           connection.close();
+       } 
+     } 
+       catch (Exception ex){
+       throw new RuntimeException("Erro ao fechar a conexão com o banco de dados");
  
  
  
